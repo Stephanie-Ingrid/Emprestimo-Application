e145505d95229421c8e4be17bd7ff995116bcfaf
stephanie ingrid <stephanieingrid5@gmail.com>
2024-09-09 15:20:36 -0300

    Validações quantidade de digitos com regex para todos os identificadores

--- src/main/java/com/grupmoney/core_emprestimo/rest/dto/IdentificadorDTO.java
+++ src/main/java/com/grupmoney/core_emprestimo/rest/dto/IdentificadorDTO.java
@@ -1,6 +1,8 @@
 package com.grupmoney.core_emprestimo.rest.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,13 +18,17 @@ import org.hibernate.validator.constraints.br.CPF;
 public class IdentificadorDTO {
 
     @CPF(message = "CPF inválido")
+    @Pattern(regexp = "^\\d{11}$", message = "CPF deve conter 11 digitos")
     private String cpf;
 
     @CNPJ(message = "CNPJ inválido")
+    @Pattern(regexp = "^\\d{14}$", message = "CNPJ deve conter 14 digitos")
     private String CNPJ;
 
+    @Pattern(regexp = "^\\d{8}$", message = "Matricula deve conter 8 digitos")
     private String matriculaEstudante;
 
+    @Pattern(regexp = "^\\d{10}$", message = "Identificador Aposentado deve conter 10 digitos")
     private String identificadorAposentado;
 
 }
